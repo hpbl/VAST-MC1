50375dcc3c4916bf9466931fcb7d6a82b5adfd57
Hilton Pintor <hpbl@cin.ufpe.br>
2017-06-01 05:52:23 -0300

    Adding tooltip to MapChart

--- Vehicles x Gates/GatesData.js
+++ Vehicles x Gates/GatesData.js
@@ -34,6 +34,10 @@ background.append("svg:image")
 var canvas = mySVG.append('g')
                   .attr("transform", "translate(100, 100) scale(3, 3)")
 
+var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
 var circles = canvas.selectAll('circle').data(gatesCarArray)
 
 circles.enter().append('circle')
@@ -41,3 +45,16 @@ circles.enter().append('circle')
                 .attr('cy', function (d) { return nodes[Object.keys(d)[0]].pos[1] })
                 .attr('r', function (d) { return Object.values(d)[0].length / 1500 })
                 .attr('fill', function (d) { return "red" })
+                .on("mouseover", function(d) {
+                   div.transition()
+                     .duration(200)
+                     .style("opacity", .9);
+                   div.html(Object.keys(d)[0] + "<br/>" + Object.values(d)[0].length)
+                     .style("left", (d3.event.pageX) + "px")
+                     .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function(d) {
+                  div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+                  });
