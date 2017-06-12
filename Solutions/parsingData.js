a4b601b3661cdbd79133fa5f10a605143595fa66
Renan Freitas <rfl3@cin.ufpe.br>
2017-06-12 02:12:54 -0300

    Organized the files into new folders structure

--- Solutions/parsingData.js
+++ Solutions/parsingData.js
@@ -13,11 +13,11 @@ Array.prototype.unique = function() {
         }
     }
     return arr;
-}
+};
 
 // Function to get unique days, and eliminating multiple occurrences in a day.
 function uniqueData(data){
-	var arr = new Array();
+	var arr = [];
 	for (j=0; j<data.length; j++) {
 		arr.push(data[j].Timestamp.substring(0,10));
 	}
@@ -26,7 +26,7 @@ function uniqueData(data){
 
 // Function to get unique type of cars, and eleminating multiple occurrences.
 function uniqueCarType(data){
-	var arr = new Array();
+	var arr = [];
 	for (j=0; j<data.length; j++) {
 		arr.push(data[j]["car-type"]);
 	}
@@ -35,7 +35,7 @@ function uniqueCarType(data){
 
 // Function get unique Ids
 function uniqueCarId(data){
-	var arr = new Array();
+	var arr = [];
 	for (j=0; j<data.length; j++) {
 		arr.push(data[j]["car-id"]);
 	}
@@ -44,14 +44,14 @@ function uniqueCarId(data){
 
 // Return each day with the cars that passes through. These days are divided by the types of the cars that passed.
 function groupData(dates, data){
-	var groupedData = new Array();
+	var groupedData = [];
 	data = crossfilter(data).dimension(function(d) { return d.Timestamp; });
 	for(i=0; i<dates.length; i++){
 		groupedData.push(data.filter([(dates[i]+" 00:00:00"), (dates[i]+" 23:59:59")]).top(Infinity));
 	}
 	for(i=0; i<groupedData.length; i++){
-		data = crossfilter(groupedData[i]).dimension(function(d) { return d["car-type"]; })
-		var groupedCars = new Array();
+		data = crossfilter(groupedData[i]).dimension(function(d) { return d["car-type"]; });
+		var groupedCars = [];
 		for(j=0; j<car.length; j++){
 			groupedCars.push(data.filter(car[j]).top(Infinity));
 		}
@@ -61,7 +61,7 @@ function groupData(dates, data){
 }
 
 function numberOfCars(dates, data){
-	var groupedData = new Array();
+	var groupedData = [];
 	data = crossfilter(data).dimension(function(d) { return d.Timestamp; });
 	for(i=0; i<dates.length; i++){
 		groupedData.push(data.filter([(dates[i]+" 00:00:00"), (dates[i]+" 23:59:59")]).top(Infinity));
